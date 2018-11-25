00b8e785ea187e286bb33f885c9510531c4d5880
Deni Cho <dencho.12@gmail.com>
2018-11-25 22:43:06 +0700

    Run detached mode for server

--- scripts/deploy_keys.sh
+++ scripts/deploy_keys.sh
@@ -3,5 +3,5 @@ sshpass -p $1 ssh -o StrictHostKeyChecking=no root@178.128.214.2 <<-'ENDSSH'
   docker stop registry.gitlab.com/denicho/unthicc/keys:master
   docker rm registry.gitlab.com/denicho/unthicc/keys:master
   docker pull registry.gitlab.com/denicho/unthicc/keys:master
-  docker run -p 80:3000 registry.gitlab.com/denicho/unthicc/keys:master
+  docker run -p 80:3000 -d registry.gitlab.com/denicho/unthicc/keys:master
 ENDSSH
