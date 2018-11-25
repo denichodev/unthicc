a88836ae4547977009f8870ff8b46cb88351da05
Deni Cho <dencho.12@gmail.com>
2018-11-25 22:56:46 +0700

    Add naming and image mgmt on deploy script

--- scripts/deploy_keys.sh
+++ scripts/deploy_keys.sh
@@ -1,7 +1,7 @@
 sshpass -p $1 ssh -o StrictHostKeyChecking=no root@178.128.214.2 <<-'ENDSSH'
   docker login -u $2 -p $3 registry.gitlab.com
-  docker stop registry.gitlab.com/denicho/unthicc/keys:master
-  docker rm registry.gitlab.com/denicho/unthicc/keys:master
+  docker stop keys
+  docker rm keys
   docker pull registry.gitlab.com/denicho/unthicc/keys:master
-  docker run -p 80:3000 -d registry.gitlab.com/denicho/unthicc/keys:master
+  docker run --name keys -p 80:3000 -d registry.gitlab.com/denicho/unthicc/keys:master
 ENDSSH
