a45b9f88fd6e071991fb5d2292218f3d4eaadbc7
Deni Cho <dencho.12@gmail.com>
2018-12-01 02:32:50 +0700

    Wrong deployment scripts

--- shared/deploy/scripts/deploy_keys.py
+++ shared/deploy/scripts/deploy_keys.py
@@ -1,23 +1,21 @@
 from fabric import Connection
+from utils import initSwarm
 
-# SCRIPTS_ROOT = "/usr/app/scripts"
 SCRIPTS_ROOT = "/usr/app/scripts"
 
 c = Connection("root@178.128.214.2")
 
 # Will return error if already initialized, just ignore the errors for now.
-c.run('docker swarm init')
+initSwarm(c)
 
 c.put(SCRIPTS_ROOT + "/docker-compose.yml", remote="/root/")
 c.put(SCRIPTS_ROOT + "/docker-compose.prod.yml", remote="/root/")
 
 result = c.run(
-    """
-    docker stack deploy
-        --compose-file docker-compose.yml
-        --compose-file docker-compose.prod.yml 
-        unthicc
-    """
+    'docker stack deploy \
+    --compose-file docker-compose.yml \
+    --compose-file docker-compose.prod.yml \
+    unthicc'
 )
 
 print('Scripts done, printing the result:')
