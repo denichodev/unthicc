2f3249375ac708b2096cd7bf497189c2fd969adf
Deni Cho <denichodev@gmail.com>
2019-02-26 05:39:18 +0700

    Remove os hostname

--- services/keys/src/routes/api/key.js
+++ services/keys/src/routes/api/key.js
@@ -1,11 +1,8 @@
-import os from 'os';
-
 const routes = async (app) => {
   app.get('/keys/ping', async () => ({
     data: {
       message: 'pong',
       version: 'testing',
-      hostname: os.hostname(),
     }
   }));
 };
