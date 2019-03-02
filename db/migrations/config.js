937f6bf3451c2483c62f866702f2f08f29ab02eb
Deni Cho <denichodev@gmail.com>
2019-03-03 01:51:51 +0700

    Remove unused db migrations

--- db/migrations/config.js
+++
@@ -1,9 +0,0 @@
-const DEFAULT_CONFIG = {
-  driver: 'rethinkdbdash',
-  db: process.env.NODE_ENV === 'test' ? 'testing' : 'unthicc',
-  host: '0.0.0.0',
-  port: 28015,
-  migrationsDirectory: 'db/migrations',
-};
-
-module.exports = DEFAULT_CONFIG;
